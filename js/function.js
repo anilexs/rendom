06de3f3b3c73439ca47e63d3da61980d73fc4544
alexis <alexissimoes94@gmail.com>
2023-06-15 23:08:46 +0200

    v1

---
+++ js/function.js
@@ -0,0 +1,10 @@
+function victim(num){
+    var elem = [];
+    var vic;
+    for(var i = 0; i < num; i++){
+        vic = prompt("");
+        elem.push(vic);
+    }
+    // var rendom = Math.floor(Math.rendom * elem.length)
+    console.log(elem.length);
+}
